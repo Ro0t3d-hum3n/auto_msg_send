ad57b78bd9266bae9c7ca16379cc46ad938d4c7b
Ro0t3d-hum3n <61425598+Ro0t3d-hum3n@users.noreply.github.com>
2024-02-07 09:19:08 +0100

    Update Romantic massage.py

--- Romantic massage.py
+++ Romantic massage.py
@@ -3,7 +3,7 @@ import time
 message = 10
 while message > 0:
     time.sleep(3)
-    pyautogui.typewrite('I am really sorry please forgive me please'
+    pyautogui.typewrite('I am really sorry please forgive me!'
                         ' ')
     time.sleep(3)
     pyautogui.press('enter')
